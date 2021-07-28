d0eaf949daa3d83133f5a1c13e844a1ab4fd9a52
Mehul Rao <mehulrao@gmail.com>
2021-07-27 22:53:59 -0400

    Working

--- Assets/Scripts/Racecar/RacecarAgent.cs
+++ Assets/Scripts/Racecar/RacecarAgent.cs
@@ -11,11 +11,32 @@ public class RacecarAgent : Agent
 
     Drive Drive;
     private Rigidbody rb;
+    public GameObject redCone1;
+    private Vector3 redCone1Pos;
+    public GameObject redCone2;
+    private Vector3 redCone2Pos;
+    public GameObject redConeFinish;
+    private Vector3 redConeFinishPos;
+    public GameObject blueCone1;
+    private Vector3 blueCone1Pos;
+    public GameObject blueCone2;
+    private Vector3 blueCone2Pos;
+    public GameObject blueConeFinish;
+    private Vector3 blueConeFinishPos;
+    public GameObject finishLine;
+    private Vector3 finishLinePos;
     public override void Initialize()
     {
         this.Drive = GetComponent<Drive>();
         this.Drive.MaxSpeed = 1;
         rb = GetComponent<Rigidbody>();
+        redCone1Pos = redCone1.transform.localPosition;
+        redCone2Pos = redCone2.transform.localPosition;
+        redConeFinishPos = redConeFinish.transform.localPosition;
+        blueCone1Pos = blueCone1.transform.localPosition;
+        blueCone2Pos = blueCone2.transform.localPosition;
+        blueConeFinishPos = blueConeFinish.transform.localPosition;
+        finishLinePos = finishLine.transform.localPosition;
     }
     public override void CollectObservations(VectorSensor sensor) {
     // Target and Agent positions
@@ -28,6 +49,7 @@ public class RacecarAgent : Agent
             EndEpisode();
         }
         else if (other.gameObject.tag == "checkpoint") {
+            Debug.Log("Checkpoint");
             AddReward(1f);
         }
         else if (other.gameObject.tag == "end") {
@@ -57,5 +79,22 @@ public class RacecarAgent : Agent
         rb.velocity = Vector3.zero;
         transform.localPosition = new Vector3(0, 0, -39);
         transform.localRotation = Quaternion.identity;
+        Vector3 newRedCone1Pos = redCone1Pos;
+        newRedCone1Pos.x += Random.Range(-5f, 5f);
+        Vector3 newRedCone2Pos = redCone2Pos;
+        newRedCone2Pos.x += Random.Range(-5f, 5f);
+        Vector3 newBlueCone1Pos = blueCone1Pos;
+        newBlueCone1Pos.x += Random.Range(-5f, 5f);
+        Vector3 newBlueCone2Pos = blueCone2Pos;
+        newBlueCone2Pos.x += Random.Range(-5f, 5f);
+        Vector3 newFinishLinePos = finishLinePos;
+        newFinishLinePos.x += Random.Range(-5f, 5f);
+        redCone1.transform.localPosition = newRedCone1Pos;
+        redCone2.transform.localPosition = newRedCone2Pos;
+        redConeFinish.transform.localPosition = redConeFinishPos;
+        blueCone1.transform.localPosition = newBlueCone1Pos;
+        blueCone2.transform.localPosition = newBlueCone2Pos;
+        blueConeFinish.transform.localPosition = blueConeFinishPos;
+        finishLine.transform.localPosition = newFinishLinePos;
     }
 }
